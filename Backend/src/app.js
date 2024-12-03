61c4101df4a156737459a90ced427556bc2ffec6
Hitanshu Variya <hjvariya1@gmail.com>
2024-12-03 13:54:34 +0530

    deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,12 +15,21 @@ import { dirname } from 'path';
 dotenv.config();
 const app = express(); 
 
-const allowedOrigins = process.env.CLIENT_URL ? [process.env.CLIENT_URL] : []; 
-console.log(allowedOrigins);
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  'https://frontend-domain.com',
+  'http://localhost:3000'
+];
 
 const corsOptions = {
-  origin: allowedOrigins,
-  credentials: true,      
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 };
 
@@ -31,8 +40,13 @@ const __dirname = dirname(__filename);
 
 app.use(express.json());  
 app.use(cookieParser());
-app.use('/public', express.static(path.join(__dirname, '../public')));
-app.use('/public/Images', express.static(path.join(__dirname, '../public/Images')));
+
+const publicPath = process.env.NODE_ENV === 'production' 
+  ? path.join(__dirname, '../public')
+  : path.join(__dirname, '../public');
+
+app.use('/public', express.static(publicPath));
+app.use('/public/Images', express.static(path.join(publicPath, 'Images')));
 
 app.use("/auth", authroutes);
 app.use("/profile", profileroutes);
@@ -40,5 +54,14 @@ app.use("/admin", adminroutes);
 app.use("/appointment", appointmentroutes);
 app.use("/patient", patientroutes);
 app.use("/", Filterroutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: 'Internal Server Error',
+    error: process.env.NODE_ENV === 'development' ? err.message : undefined
+  });
+});
 
 export default app; 
