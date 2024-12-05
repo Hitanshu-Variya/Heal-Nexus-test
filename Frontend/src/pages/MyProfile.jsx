a904436f5ccf03adb213b2875ecb380cec076a21
chaudhariayush <chaudhariayush3200@gmail.com>
2024-12-05 15:27:28 +0530

    final commit

--- Frontend/src/pages/MyProfile.jsx
+++ Frontend/src/pages/MyProfile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { toast } from 'react-toastify';
+import { toast } from 'react-hot-toast';
 import { Appbar } from './dashBoard';
 import { z } from 'zod';
 
